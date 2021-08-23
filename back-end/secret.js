cf08b5afc14d39be271f07e321ac84b5d686ab31
kevin820125 <71917261+kevin820125@users.noreply.github.com>
2021-08-23 14:48:44 -0700

    Delete secret.js

--- back-end/secret.js
+++
@@ -1,6 +0,0 @@
-url = 'mongodb+srv://kevin:K199723k@cluster1.mkerl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
-// mongodb+srv://kevin:K199723k@cluster1.mkerl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-
-
-module.exports = url
